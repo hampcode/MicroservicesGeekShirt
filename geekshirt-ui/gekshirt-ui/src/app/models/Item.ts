3fd4664d08965e7067ff13184552c8966ccd79ba
joseflo <josefloresingaruca@gmail.com>
2021-06-09 17:52:09 -0500

    [Frontend] Cambiado por interface

--- geekshirt-ui/gekshirt-ui/src/app/models/Item.ts
+++ geekshirt-ui/gekshirt-ui/src/app/models/Item.ts
@@ -1,14 +1,6 @@
-
-export  class Item{
+export  interface Item{
   upc:number;
   quantity: number;
   price:number;
 
-  constructor( upc:number,  quantity: number,price:number){
-    this.upc=upc;
-    this.quantity=quantity;
-    this.price=price;
-
-  }
 }
-
