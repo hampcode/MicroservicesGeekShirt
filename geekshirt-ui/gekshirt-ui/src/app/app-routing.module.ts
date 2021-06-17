7cc9054a70e499360d7bff76c1f14d96f0566758
joseflo <josefloresingaruca@gmail.com>
2021-06-16 22:29:16 -0500

    [Frontend] Incorporación del componente LoginComponent dentro del Routes

--- geekshirt-ui/gekshirt-ui/src/app/app-routing.module.ts
+++ geekshirt-ui/gekshirt-ui/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {OrderListComponent} from "./components/order/order-list/order-list.component";
 import {OrderAddComponent} from "./components/order/order-add/order-add.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
+  {path:'login',component:LoginComponent},
   {path:'orders',component:OrderListComponent},
   {path:'orderAdd',component:OrderAddComponent}
 ];
